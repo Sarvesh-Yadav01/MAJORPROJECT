8737d9a17317bf2d568ec88b15e1935132ba7c7e
Sarvesh-Yadav01 <yadavsarvesh93056@gmail.com>
2026-03-15 09:18:07 +0530

    create database

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
+//connect to MongoDB or database
+const MONGO_URL = 'mongodb:127.0.0.1:27017/Wanderlust';
+main().then(() => {
+    console.log('Connected to MongoDB');
+}).catch((err) => {
+    console.log(err);
+});
+
+async function main() {
+    await mongoose.connect(MONGO_URL);
+}
+
 //create a home API
 app.get('/', (req, res) => {
     res.send('Welcome to the Home API or root');
