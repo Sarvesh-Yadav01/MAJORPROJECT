d4bc3eb1b3947059c6438867bc8d648fef96413e
Sarvesh-Yadav01 <yadavsarvesh93056@gmail.com>
2026-03-15 08:58:18 +0530

    create a sever

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const mongoose = require('mongoose');
+
+app.listen (8080, () => {
+    console.log('Server is running on port 8080');
+});
