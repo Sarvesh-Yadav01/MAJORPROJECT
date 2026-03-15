ce3ad2c8bb68a48e5aa21b4fbb4d52f797779cac
Sarvesh-Yadav01 <yadavsarvesh93056@gmail.com>
2026-03-15 09:02:47 +0530

    create api

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
+//create a home API
+app.get('/', (req, res) => {
+    res.send('Welcome to the Home API or root');
+});
+
+//creating a server
 app.listen (8080, () => {
     console.log('Server is running on port 8080');
 });
